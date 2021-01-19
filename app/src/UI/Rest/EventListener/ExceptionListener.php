15bd812f8e6451fe350834a956c5685a5f0abc75
Gievoi Viktor <viktor.gievoi@pimpay.ru>
2021-01-19 17:24:06 +0300

    Fix stage

--- app/src/UI/Rest/EventListener/ExceptionListener.php
+++ app/src/UI/Rest/EventListener/ExceptionListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\UI\Rest\EventListener;
 
+use App\Domain\SystemCodes;
 use App\UI\Rest\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,7 +32,19 @@ final class ExceptionListener
     {
         return
             match ($exception->getCode()) {
-                default => Response::HTTP_INTERNAL_SERVER_ERROR
+                SystemCodes::SHOPPING_LIST_CREATION_ERROR   => Response::HTTP_INTERNAL_SERVER_ERROR,
+                SystemCodes::PERMISSION_DENIED              => Response::HTTP_FORBIDDEN,
+                SystemCodes::UNIT_NOT_FOUND                 => Response::HTTP_NOT_FOUND,
+                SystemCodes::LABEL_NOT_FOUND                => Response::HTTP_NOT_FOUND,
+                SystemCodes::IMAGE_NOT_FOUND                => Response::HTTP_NOT_FOUND,
+                SystemCodes::CATEGORY_NOT_FOUND             => Response::HTTP_NOT_FOUND,
+                SystemCodes::PERSON_NOT_FOUND               => Response::HTTP_NOT_FOUND,
+                SystemCodes::TEMPLATE_NOT_FOUND             => Response::HTTP_NOT_FOUND,
+                SystemCodes::LIST_NOT_FOUND                 => Response::HTTP_NOT_FOUND,
+                SystemCodes::ALREADY_EXISTS                 => Response::HTTP_NOT_FOUND,
+                SystemCodes::ITEM_NOT_FOUND                 => Response::HTTP_NOT_FOUND,
+                SystemCodes::POINT_NOT_FOUND                => Response::HTTP_NOT_FOUND,
+                default                                     => Response::HTTP_INTERNAL_SERVER_ERROR
             }
         ;
     }
