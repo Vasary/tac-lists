9c8cdf89c13d0fae491bd320cefbf1a74d2eff82
Gievoi Viktor <viktor.gievoi@pimpay.ru>
2021-01-20 13:17:19 +0300

    Tracing bundle added

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Ekreative\HealthCheckBundle\EkreativeHealthCheckBundle::class => ['all' => true],
+    Vasary\TracingBundle\TracingBundle::class => ['all' => true],
 ];
